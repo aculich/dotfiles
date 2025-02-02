e361f641bf21c417569103aef655cb40f0ba78fd
Aaron Culich <aculich@gmail.com>
2025-02-02 08:13:03 -0800

    style: update gpdl alias for improved git log range
    - Modify the gpdl alias to use @{push}..HEAD for git log
    - This change ensures the log shows commits not yet pushed
    - Improves clarity and usability of the alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -93,7 +93,7 @@ alias gi='git init'       # Initialize git repo
 alias gcom='git gcommit'  # Custom git commit
 alias gcoma='git ac'      # Git add and commit
 alias ged='git diff HEAD~1 | llm -m 4o-mini -s "explain changes from last commit"'
-alias gpdl='git push --dry-run && git log --oneline --decorate -n 5 @{push}..'
+alias gpdl='git push --dry-run && git log --oneline --decorate @{push}..HEAD'
 
 alias pc='pbcopy'
 
