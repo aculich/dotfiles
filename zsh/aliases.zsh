957076c2ebb13a8c64cc6671e641a142844b7562
Aaron Culich <aculich@gmail.com>
2025-02-14 12:10:00 -0800

    move ghrepos above other aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -63,6 +63,36 @@ alias grp='git remote get-url origin | pbcopy; pbpaste'                         
 alias gitpullall='for d in */; do echo -n "$d..."; (cd "$d" && git pull --all); done'                 # Pull all repos in current dir
 alias gclones='for url in $(<urls.list); do echo $i; git clone "$url" "${url:t}__${url:h:t}" ; done'  # Clone repos from urls.list with namespaced dirs
 
+alias ghrepos='
+local f
+f() {
+  # Save the original directory
+  local orig_dir="$(pwd)"
+  
+  # Extract the owner name if the argument is a URL
+  local OWNER="$1"
+  [[ "$OWNER" =~ ^https?://(www\.)?github\.com/(.+)/?$ ]] && OWNER="${BASH_REMATCH[2]}"
+  
+  # Create a directory for the owner and change into it
+  mkdir -p "$OWNER"
+  cd "$OWNER" || return
+  
+  # List the repositories and process the output
+  gh repo list "$OWNER" --json name,description,url,createdAt,updatedAt,stargazerCount,forkCount,languages,owner --limit 100 |
+    tee repos.json |
+    jq ".[].url" -r |
+    tee repos.list
+  
+  echo "Created $OWNER/repos.list - run \"cd $OWNER && gcll\" to clone all repos"
+  
+  # Return to the original directory
+  cd "$orig_dir" || return
+  
+  unset -f f
+}
+f
+'
+
 # Clone a GitHub repository with branches in separate directories
 # Usage: ghdirbranch owner/repo or ghdirbranch https://github.com/owner/repo
 # Pros:
@@ -132,37 +162,6 @@ alias ghworkbranch='local f; f() {
     # Return to original directory
     cd .. || return
 }; f'
-
-alias ghrepos='
-local f
-f() {
-  # Save the original directory
-  local orig_dir="$(pwd)"
-  
-  # Extract the owner name if the argument is a URL
-  local OWNER="$1"
-  [[ "$OWNER" =~ ^https?://(www\.)?github\.com/(.+)/?$ ]] && OWNER="${BASH_REMATCH[2]}"
-  
-  # Create a directory for the owner and change into it
-  mkdir -p "$OWNER"
-  cd "$OWNER" || return
-  
-  # List the repositories and process the output
-  gh repo list "$OWNER" --json name,description,url,createdAt,updatedAt,stargazerCount,forkCount,languages,owner --limit 100 |
-    tee repos.json |
-    jq ".[].url" -r |
-    tee repos.list
-  
-  echo "Created $OWNER/repos.list - run \"cd $OWNER && gcll\" to clone all repos"
-  
-  # Return to the original directory
-  cd "$orig_dir" || return
-  
-  unset -f f
-}
-f
-'
-
 
 alias ghfork='local f; f() { repo=$1; owner=$(basename $(dirname "$repo")); name=$(basename "$repo"); gh repo fork "$repo" --clone; mv "$name" "${name}__${owner}"; }; f'  # Fork and clone with namespaced dir
 alias gcl='local f; f() { url=$1; git clone "$url" "${url:t}"; }; f'
