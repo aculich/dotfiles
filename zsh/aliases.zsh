5bc54bf0ba230915648eaccafbe5ff87a82635d5
Aaron Culich <aculich@gmail.com>
2025-03-06 02:18:34 -0800

    feat: add gclo alias for structured git clone
    - Introduce 'gclo' alias to clone repositories into a directory
      structure that includes the parent directory of the URL.
    - This provides a more organized way to clone repositories,
      especially when dealing with multiple repositories from
      different sources.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -224,6 +224,7 @@ alias ghworkbranch='local f; f() {
 
 alias ghfork='local f; f() { repo=$1; owner=$(basename $(dirname "$repo")); name=$(basename "$repo"); gh repo fork "$repo" --clone; mv "$name" "${name}__${owner}"; }; f'  # Fork and clone with namespaced dir
 alias gcl='local f; f() { url=$1; git clone "$url" "${url:t}"; }; f'
+alias gclo='local f; f() { url=$1; git clone "$url" "${url:h:t}/${url:t}"; }; f'
 alias gcls='local f; f() { url=$1; git clone --depth=1 --no-single-branch "$url" "${url:t}"; }; f'
 gcll() { local filename="${1:-repos.list}"; while read -r i; do echo "$i"; gcls "$i"; done < "$filename"; }  # Clone all repos from list file
 alias cls="chrome-cli list links | fzf"
